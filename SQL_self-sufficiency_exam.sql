a376289a87ec3b570839408bc9ca826ea7ca3910
fazel74 <53278218+fazel74@users.noreply.github.com>
2019-09-30 23:55:35 -0700

    Update SQL_self-sufficiency_exam.sql

--- SQL_self-sufficiency_exam.sql
+++ SQL_self-sufficiency_exam.sql
@@ -19,7 +19,7 @@ FROM naep
 LIMIT 50;
 
 -- 3) Write a query that returns summary statistics for avg_math_4_score by state. Make sure to sort the results 
-      alphabetically by state name.
+--    alphabetically by state name.
 
 SELECT COUNT(avg_math_4_score), ROUND(AVG(avg_math_4_score), 3), MAX(avg_math_4_score), MIN(avg_math_4_score), state
 FROM naep
@@ -28,7 +28,7 @@ GROUP BY state
 ORDER BY state;
 
 -- 4) Write a query that alters the previous query so that it returns only the summary statistics for avg_math_4_score 
-      by state with differences in max and min values that are greater than 30.
+--    by state with differences in max and min values that are greater than 30.
 
 SELECT COUNT(avg_math_4_score), ROUND(AVG(avg_math_4_score), 3), MAX(avg_math_4_score), MIN(avg_math_4_score), state
 , (MAX(avg_math_4_score) - MIN(avg_math_4_score)) AS differences_in_max_and_min
@@ -44,7 +44,7 @@ ORDER BY state;
 "11"	"237.487"	"244.802"	"212.884"	"NORTH_CAROLINA"	"31.918"      
                                                                                                                                                    
 -- 5) Write a query that returns a field called bottom_10_states that lists the states in the bottom 10 for avg_math_4_score 
-      in the year 2000.
+--    in the year 2000.
 
 SELECT state AS bottom_10_states
 FROM naep
@@ -63,7 +63,7 @@ LIMIT 10;
 "GEORGIA"
                                                                                                                                                    
 -- 6) Write a query that calculates the average avg_math_4_score rounded to the nearest 2 decimal places over all states in 
-      the year 2000.
+--    the year 2000.
                                                                                                                                              
 SELECT state, ROUND(avg_math_4_score, 2) AS avg_math_4_score
 FROM naep
@@ -71,7 +71,7 @@ WHERE year = 2000
 ORDER BY state;
                                                                                                                                             
 -- 7) Write a query that returns a field called below_average_states_y2000 that lists all states with an avg_math_4_score 
-      less than the average over all states in the year 2000.
+--    less than the average over all states in the year 2000.
 
 SELECT state AS below_average_states_y2000, avg_math_4_score
 FROM naep
@@ -100,7 +100,7 @@ HAVING avg_math_4_score < (
 "KENTUCKY"	"220.994"
                                                                                                                                            -- 8) Write a query that returns a field called scores_missing_y2000 that lists any states with missing values in the avg_math_4_score column of the naep data table for the year 2000.
 -- 8) Write a query that returns a field called scores_missing_y2000 that lists any states with missing values in 
-      the avg_math_4_score column of the naep data table for the year 2000.
+--    the avg_math_4_score column of the naep data table for the year 2000.
                                                         
 SELECT state AS scores_missing_y2000
 FROM naep
@@ -118,9 +118,9 @@ WHERE year = 2000 AND avg_math_4_score IS null;
 "WISCONSIN"
         
 -- 9) Write a query that returns for the year 2000 the state, avg_math_4_score, and total_expenditure from the naep 
-      table left outer joined with the finance table, using id as the key and ordered by total_expenditure greatest to least. 
-      Be sure to round avg_math_4_score to the nearest 2 decimal places, and then filter out NULL avg_math_4_scores in order to 
-      see any correlation more clearly.
+--    table left outer joined with the finance table, using id as the key and ordered by total_expenditure greatest to least. 
+--    Be sure to round avg_math_4_score to the nearest 2 decimal places, and then filter out NULL avg_math_4_scores in order to 
+--    see any correlation more clearly.
                                                                                                                                            
 SELECT naep.state, ROUND(avg_math_4_score, 2) AS avg_score, total_expenditure
 FROM naep LEFT OUTER JOIN finance
